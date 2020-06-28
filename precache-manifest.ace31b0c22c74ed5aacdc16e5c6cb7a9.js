fa55ab9c364ad13505cb4ca8d4aef2ceacf586fc
Jocelyn James <jocelynjames@Jocelyns-MacBook-Pro.local>
2020-06-28 09:31:29 -0400

    Updates

---
+++ precache-manifest.ace31b0c22c74ed5aacdc16e5c6cb7a9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b855ad0c1ffbf539e652ace2a1d33be",
+    "url": "/Fast_search/index.html"
+  },
+  {
+    "revision": "04af57f74b6b414aea55",
+    "url": "/Fast_search/static/css/main.bd621a36.chunk.css"
+  },
+  {
+    "revision": "09de91fdc5a2eef800a5",
+    "url": "/Fast_search/static/js/2.2e34a01c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Fast_search/static/js/2.2e34a01c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04af57f74b6b414aea55",
+    "url": "/Fast_search/static/js/main.bac171ae.chunk.js"
+  },
+  {
+    "revision": "547aadc06f172e3f2f58",
+    "url": "/Fast_search/static/js/runtime-main.e0ab2186.js"
+  },
+  {
+    "revision": "7750b98e5056dd5a09aa140b2c759e95",
+    "url": "/Fast_search/static/media/fast_logo.7750b98e.svg"
+  },
+  {
+    "revision": "06a523d0872c39a1a01187d7cae92342",
+    "url": "/Fast_search/static/media/question_mark.06a523d0.png"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/Fast_search/precache-manifest.ace31b0c22c74ed5aacdc16e5c6cb7a9.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Fast_search/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
